29f047d22f8e2c485d778b8df5a941c9b9578cee
perosm <marko.peros55@gmail.com>
2025-04-13 17:35:57 +0200

    minor changes

--- utils/kitti/folder_structure/object_detection_3d.py
+++ utils/kitti/folder_structure/object_detection_3d.py
@@ -4,33 +4,59 @@ import pathlib
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from matplotlib import axes
+from utils.visual_inspection import project_3d_bbox_to_image, draw_3d_bbox, draw_bbox
 
 
 def main():
     args = _parse_args()
 
-    input_image_paths = sorted(
-        [file for file in args.data_object_detection_3d.iterdir()]
-    )
-    ground_truth_paths = sorted(
-        [file for file in args.label_object_detection_3d.iterdir()]
-    )
-    calibration_paths = sorted(
-        [file for file in args.object_detection_3d_calibration.iterdir()]
-    )
-    for input_image_path, ground_truth_path, calibration_path in zip(
-        input_image_paths, ground_truth_paths, calibration_paths
-    ):
-        visual_inspection(input_image_path, ground_truth_path, calibration_path)
-        # full_source_path_name.rename(full_destination_path_name / image_name)
+    input_images_dir = args.input_images_dir
+    mapping_path = args.devkit_object_detection_3d_mapping
+    ground_truth_dir = args.label_object_detection_3d
+    calibration_dir = args.object_detection_3d_calibration
+    destination_root_folder = args.destination_root_folder
 
-    # shutil.rmtree(args.data_object_detection_3d.parent.parent)
-    # shutil.rmtree(args.devkit_object_detection_3d_mapping.parent.parent)
-    # num_files = sum(1 for _ in destination_root_dir_name.rglob("*") if _.is_file())
+    with open(mapping_path / "train_mapping.txt", "r") as file:
+        mapping_info = [line.strip().split(" ") for line in file.readlines()]
 
-    # print(f"Number of files: {num_files}")
-    # print(f"Number of mappings: {len(lines)}")
+    with open(mapping_path / "train_rand.txt", "r") as file:
+        mapping_info_indexes = [
+            int(number) for number in file.readline().strip().split(",")
+        ]
+    ground_truth_paths = sorted([file for file in ground_truth_dir.iterdir()])
+    calibration_paths = sorted([file for file in calibration_dir.iterdir()])
+
+    for (
+        frame_index,
+        ground_truth_path,
+        calibration_path,
+    ) in zip(mapping_info_indexes, ground_truth_paths, calibration_paths):
+        date, drive, frame = mapping_info[frame_index - 1]
+        subfolders = pathlib.Path(f"{date}/{drive}/image_02/data")
+        # input_image_path = input_images_dir / subfolders / f"{frame}.png"
+        # visual_inspection(input_image_path, ground_truth_path, calibration_path)
+        ground_truth_destination_dir = (
+            destination_root_folder / "ground_truth" / subfolders
+        )
+        ground_truth_destination_dir.mkdir(parents=True, exist_ok=True)
+        calibration_destination_dir = (
+            destination_root_folder / "calibrations" / subfolders
+        )
+        calibration_destination_dir.mkdir(parents=True, exist_ok=True)
+        ground_truth_path.replace(
+            ground_truth_destination_dir / f"{frame}{ground_truth_path.suffix}"
+        )
+        calibration_path.replace(
+            calibration_destination_dir / f"{frame}.{calibration_path.suffix}"
+        )
+    breakpoint()
+    shutil.rmtree(mapping_path.parent)
+    shutil.rmtree(ground_truth_dir.parent.parent)
+    shutil.rmtree(calibration_dir.parent.parent)
+    num_files = sum(1 for _ in destination_root_folder.rglob("*.txt") if _.is_file())
+
+    print(f"Number of files: {num_files / 2}")
+    print(f"Number of mappings: {len(mapping_info_indexes)}")
 
 
 def visual_inspection(
@@ -49,14 +75,14 @@ def visual_inspection(
             [int(float(image_coords)) for image_coords in object_info[4:8]]
         )
         bbox_3d = np.array([float(world_coords) for world_coords in object_info[8:15]])
-        _draw_bbox(image_2d_bboxes, bbox_2d)
-        projected_points = _project_3d_bbox_to_image(bbox_3d, projection_matrix)
-        _draw_3d_bbox(image_3d_bboxes, projected_points)
+        draw_bbox(image_2d_bboxes, bbox_2d)
+        projected_points = project_3d_bbox_to_image(bbox_3d, projection_matrix)
+        draw_3d_bbox(image_3d_bboxes, projected_points)
 
     ax[0].imshow(image_2d_bboxes)
     ax[1].imshow(image_3d_bboxes)
 
-    def on_key(event):
+    def on_key():
         plt.close()
 
     plt.tight_layout()
@@ -89,77 +115,14 @@ def _read_projection_matrix(object_detection_calibration_path: pathlib.Path):
     return projection_matrix
 
 
-def _draw_bbox(image: np.ndarray, bbox: np.ndarray):
-    left, top, right, bottom = bbox
-    # Note: cv2 plots coordinates differently
-    cv2.line(image, (left, top), (right, top), color=(255, 0, 0), thickness=1)
-    cv2.line(image, (left, top), (left, bottom), color=(255, 0, 0), thickness=1)
-    cv2.line(image, (right, top), (right, bottom), color=(255, 0, 0), thickness=1)
-    cv2.line(image, (left, bottom), (right, bottom), color=(255, 0, 0), thickness=1)
-
-
-def _project_3d_bbox_to_image(bbox_3d: np.ndarray, projection_matrix: np.ndarray):
-    h, w, l = bbox_3d[0:3]
-    x, y, z = bbox_3d[3:6]
-    rotation_y = bbox_3d[6]
-    R = np.array(
-        [
-            [np.cos(rotation_y), 0, np.sin(rotation_y)],
-            [0, 1, 0],
-            [-np.sin(rotation_y), 0, np.cos(rotation_y)],
-        ]
-    )
-    x_corners = np.array([l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2])
-    y_corners = np.array([0, 0, 0, 0, -h, -h, -h, -h])
-    z_corners = np.array([w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2])
-    corners = np.vstack([x_corners, y_corners, z_corners])
-    corners = R @ corners
-    corners[0, :] += x
-    corners[1, :] += y
-    corners[2, :] += z
-    corners_homogeneous = np.vstack([corners, np.ones(shape=(1, corners.shape[1]))])
-    projections = projection_matrix @ corners_homogeneous
-    projections = projections[:2, :] / projections[2, :]
-
-    return projections.T.astype(np.int16)
-
-
-def _draw_3d_bbox(image_3d_bboxes: np.ndarray, projected_points: np.ndarray):
-    edges = [
-        # bottom
-        (0, 1),
-        (1, 2),
-        (2, 3),
-        (3, 0),
-        # top
-        (4, 5),
-        (5, 6),
-        (6, 7),
-        (7, 4),
-        # vertical
-        (0, 4),
-        (1, 5),
-        (2, 6),
-        (3, 7),
-    ]
-    for start, end in edges:
-        start_point = projected_points[start]
-        end_point = projected_points[end]
-        cv2.line(
-            image_3d_bboxes, start_point, end_point, color=(255, 0, 0), thickness=1
-        )
-
-
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-dod",
-        "--data-object-detection-3d",
+        "-iid",
+        "--input-images-dir",
         type=pathlib.Path,
-        default=pathlib.Path(
-            "./data/kitti/data_object_image_2/training/image_2"
-        ),  # left camera images
-        help="Path to 3d object detection input images.",
+        default=pathlib.Path("./data/kitti/input"),  # left camera images
+        help="Path to input images root folder.",
     )
     parser.add_argument(
         "-lod",
@@ -178,6 +141,20 @@ def _parse_args() -> argparse.Namespace:
             "./data/kitti/data_object_calib/training/calib"
         ),  # left camera images
         help="Path to 3d object detection calibration files.",
+    )
+    parser.add_argument(
+        "-drf",
+        "--destination-root-folder",
+        type=pathlib.Path,
+        default=pathlib.Path("./data/kitti/object_detection_3d"),
+        help="Destination root folder for all the corresponding files.",
+    )
+    parser.add_argument(
+        "-dodm",
+        "--devkit-object-detection-3d-mapping",
+        type=pathlib.Path,
+        default=pathlib.Path("./data/kitti/devkit_object/mapping"),
+        help="Path to mapping folder. Contains information how to map labels to raw data.",
     )
     return parser.parse_args()
 
--- utils/visual_inspection.py
+++ utils/visual_inspection.py
@@ -64,3 +64,64 @@ def plot_task_gt(task_groundtruth: dict[str, torch.Tensor]):
     plt.tight_layout()
     fig.canvas.mpl_connect("key_press_event", on_key)
     plt.show()
+
+
+def draw_bbox(image: np.ndarray, bbox: np.ndarray):
+    left, top, right, bottom = bbox
+    # Note: cv2 plots coordinates differently
+    cv2.line(image, (left, top), (right, top), color=(255, 0, 0), thickness=1)
+    cv2.line(image, (left, top), (left, bottom), color=(255, 0, 0), thickness=1)
+    cv2.line(image, (right, top), (right, bottom), color=(255, 0, 0), thickness=1)
+    cv2.line(image, (left, bottom), (right, bottom), color=(255, 0, 0), thickness=1)
+
+
+def project_3d_bbox_to_image(bbox_3d: np.ndarray, projection_matrix: np.ndarray):
+    h, w, l = bbox_3d[0:3]
+    x, y, z = bbox_3d[3:6]
+    rotation_y = bbox_3d[6]
+    R = np.array(
+        [
+            [np.cos(rotation_y), 0, np.sin(rotation_y)],
+            [0, 1, 0],
+            [-np.sin(rotation_y), 0, np.cos(rotation_y)],
+        ]
+    )
+    x_corners = np.array([l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2])
+    y_corners = np.array([0, 0, 0, 0, -h, -h, -h, -h])
+    z_corners = np.array([w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2])
+    corners = np.vstack([x_corners, y_corners, z_corners])
+    corners = R @ corners
+    corners[0, :] += x
+    corners[1, :] += y
+    corners[2, :] += z
+    corners_homogeneous = np.vstack([corners, np.ones(shape=(1, corners.shape[1]))])
+    projections = projection_matrix @ corners_homogeneous
+    projections = projections[:2, :] / projections[2, :]
+
+    return projections.T.astype(np.int16)
+
+
+def draw_3d_bbox(image_3d_bboxes: np.ndarray, projected_points: np.ndarray):
+    edges = [
+        # bottom
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 0),
+        # top
+        (4, 5),
+        (5, 6),
+        (6, 7),
+        (7, 4),
+        # vertical
+        (0, 4),
+        (1, 5),
+        (2, 6),
+        (3, 7),
+    ]
+    for start, end in edges:
+        start_point = projected_points[start]
+        end_point = projected_points[end]
+        cv2.line(
+            image_3d_bboxes, start_point, end_point, color=(255, 0, 0), thickness=1
+        )
