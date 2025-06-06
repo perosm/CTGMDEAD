1addaa9b04bc15801d4512d352d678b5a2e167c4
perosm <marko.peros55@gmail.com>
2025-06-06 23:14:18 +0200

    added data download/folder setup scripts for server

--- utils/kitti/folder_structure/depth_structure.py
+++ utils/kitti/folder_structure/depth_structure.py
@@ -1,70 +1,81 @@
-import os
+""" """
+
 import shutil
+import pathlib
+import argparse
 
-# root folder is master-thesis
-root_folder = os.path.join(os.getcwd(), "data", "kitti")  # /master-thesis/data/kitti
-root_depth_train = os.path.join(root_folder, "train")  # /master-thesis/data/kitti/train
-root_depth_val = os.path.join(root_folder, "val")  # /master-thesis/data/kitti/val
-new_root_depth = os.path.join(root_folder, "depth")  # /master-thesis/data/kitti/depth
 
-"""
-moving image_02 & image_03 folders
-from /train/20xx_xx_xx_drive_xxxx_sync/proj_depth/groundtruth
-to   /train/20xx_xx_xx_drive_xxxx_sync
-and adding /data folder to them & moving all files to /data from /[image02 or image03] folder
-"""
-for root_depth_tv in [root_depth_train, root_depth_val]:
-    if os.path.exists(root_depth_tv):
-        for subdir in sorted(os.listdir(root_depth_tv)):
-            drive_dir = os.path.join(root_depth_tv, subdir)
-            groundtruth_dir = os.path.join(drive_dir, "proj_depth", "groundtruth")
-            if os.path.isdir(groundtruth_dir):
-                for folder in ["image_02", "image_03"]:
-                    source_path = os.path.join(groundtruth_dir, folder)
-                    target_path = drive_dir
-                    if os.path.exists(target_path):
-                        shutil.move(source_path, target_path)
-                    target_data_path = os.path.join(target_path, folder, "data")
-                    if not os.path.isdir(target_data_path):
-                        os.makedirs(target_data_path)
-                    target_img_folder = os.path.join(target_path, folder)
-                    for item in sorted(os.listdir(target_img_folder)):
-                        shutil.move(
-                            os.path.join(target_img_folder, item), target_data_path
-                        )
+def main():
+    args = _parse_args()
+    root_depth_path = args.root_depth_path
+    new_root_depth_path = args.new_root_depth_path
+    move_camera_02_depth_files_and_remove_unecessary(
+        root_depth_path=root_depth_path, new_root_depth_path=new_root_depth_path
+    )
 
-drives = [
-    "2011_09_26",
-    "2011_09_28",
-    "2011_09_29",
-    "2011_09_30",
-    "2011_10_03",
-]
 
-"""
-moving folders /image02 and /image03
-from /train/20xx_xx_xx_drive_xxxx_sync
-to   /depth/train/20xx_xx_xx/20xx_xx_xx_drive_xxxx_sync
-"""
-for drive in sorted(drives):
-    for folder in ["train", "val"]:
-        drive_dir = os.path.join(new_root_depth, folder, drive)
-        if not os.path.isdir(drive_dir):
-            os.makedirs(drive_dir)
-        source_folders = os.path.join(root_folder, folder)
-        if os.path.exists(source_folders):
-            for sf in sorted(os.listdir(source_folders)):
-                if drive in sf:
-                    shutil.move(os.path.join(source_folders, sf), drive_dir)
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-rdp",
+        "--root-depth-path",
+        default=pathlib.Path("./data/kitti/data_depth_annotated"),
+        type=pathlib.Path,
+    )
+    parser.add_argument(
+        "-nrdp",
+        "--new-root-depth-path",
+        default=pathlib.Path("./data/kitti/depth"),
+        type=pathlib.Path,
+    )
+    return parser.parse_args()
 
-if os.path.exists(root_depth_train):
-    os.rmdir(root_depth_train)
-if os.path.exists(root_depth_val):
-    os.rmdir(root_depth_val)
 
-# needs to be ran 2 times because proj_depth/groundtruth
-for i in range(2):
-    for root, dirs, files in os.walk(new_root_depth):
-        for dir in dirs:
-            if not os.listdir(os.path.join(root, dir)):
-                os.rmdir(os.path.join(root, dir))
+def move_camera_02_depth_files_and_remove_unecessary(
+    root_depth_path: pathlib.Path, new_root_depth_path: pathlib.Path
+) -> None:
+    """
+    Moves 'image_02' ground truth depth directories from their original KITTI structure.
+    After moving, the original root depth path is completely removed.
+
+    This function expects the original depth files to be organized like:
+    `root_depth_path/{train,val}/<date_drive_sequence>/proj_depth/groundtruth/image_02`
+    (e.g., `data_depth_annotated/train/2011_09_26_drive_0001_sync/proj_depth/groundtruth/image_02`)
+
+    It reorganizes them into a new structure:
+    `new_root_depth_path/<date_drive>/<date_drive_sequence>/image_02/data`
+    (e.g., `depth/2011_09_26/2011_09_26_drive_0001_sync/image_02/data`)
+
+    Args:
+        root_depth_path: The absolute or relative path to the original root directory containing the KITTI depth annotations.
+                         This directory will be deleted upon successful completion.
+        new_root_depth_path: The absolute or relative path to the desired new root directory for the
+                             reorganized depth data. Parent directories will be created if they don't exist.
+    """
+
+    new_root_depth_path.mkdir(parents=True, exist_ok=True)
+
+    for dataset in ["train", "val"]:
+        dataset_root_depth_path = root_depth_path / dataset
+        for drive_dir in sorted(dataset_root_depth_path.iterdir()):
+            drive_dir_name = drive_dir.stem
+            date_dir = "_".join(drive_dir_name.split("_")[:3])
+            source_path = pathlib.Path(
+                drive_dir, "proj_depth", "groundtruth", "image_02"
+            )
+            target_path = pathlib.Path(
+                new_root_depth_path,
+                date_dir,
+                drive_dir_name,
+                "image_02",
+                "data",
+            )
+            target_path.mkdir(parents=True, exist_ok=True)
+            if source_path.exists():
+                shutil.move(source_path, target_path)
+
+    shutil.rmtree(root_depth_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/kitti/folder_structure/input_structure.py
@@ -0,0 +1,69 @@
+""" """
+
+import shutil
+import pathlib
+import argparse
+
+
+def main():
+    args = _parse_args()
+    root_depth_path = args.root_input_path
+
+    move_camera_02_input_files_and_remove_unecessary(root_input_path=root_depth_path)
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-rdp",
+        "--root-input-path",
+        default=pathlib.Path("./data/kitti/input"),
+        type=pathlib.Path,
+    )
+    return parser.parse_args()
+
+
+def move_camera_02_input_files_and_remove_unecessary(
+    root_input_path: pathlib.Path,
+) -> None:
+    """
+    Moves 'image_02' ground truth depth directories from their original KITTI structure.
+    After moving, the original root depth path is completely removed.
+
+    This function expects the original depth files to be organized like:
+    `root_depth_path/{train,val}/<date_drive_sequence>/proj_depth/groundtruth/image_02`
+    (e.g., `data_depth_annotated/train/2011_09_26_drive_0001_sync/proj_depth/groundtruth/image_02`)
+
+    It reorganizes them into a new structure:
+    `new_root_depth_path/<date_drive>/<date_drive_sequence>/image_02/data`
+    (e.g., `depth/2011_09_26/2011_09_26_drive_0001_sync/image_02/data`)
+
+    Args:
+        root_depth_path: The absolute or relative path to the original root directory containing the KITTI depth annotations.
+                         This directory will be deleted upon successful completion.
+        new_root_depth_path: The absolute or relative path to the desired new root directory for the
+                             reorganized depth data. Parent directories will be created if they don't exist.
+    """
+    initial_dirs = sorted(root_input_path.iterdir())
+    for curr_dir in initial_dirs:
+        date = "_".join(curr_dir.stem.split("_")[:3])  # year_month_day
+        if "calib" in str(curr_dir):
+            calib_destination_dir = root_input_path / date  # input/year_month_day
+            source_dir = (
+                curr_dir / date
+            )  # year_month_day_drive_xxxx_sync/year_month_day
+            shutil.move(source_dir, calib_destination_dir)
+        else:
+            drive = curr_dir.stem
+            image_destination_dir = root_input_path / date / drive / "image_02" / "data"
+            source_dir = (
+                curr_dir / date / curr_dir.stem / "image_02" / "data"
+            )  # year_month_day_drive_xxxx_sync/year_month_day/year_month_day_drive_xxxx_sync
+            shutil.move(source_dir, image_destination_dir)
+
+    for dir in initial_dirs:
+        shutil.rmtree(dir)
+
+
+if __name__ == "__main__":
+    main()
